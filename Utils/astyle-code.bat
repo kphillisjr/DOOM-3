bb453d6ba9d33571a942bec4244920ec21f5ffb4
Kenney Phillis <kphillisjr@gmail.com>
2012-12-18 19:15:49 -0600

    Remaining changes. This includes a few more deleted msvc project files.

--- Utils/astyle-code.bat
+++ Utils/astyle-code.bat
@@ -1,5 +1,7 @@
 @echo off
 @echo "Applying uniform Style to Source and Header files"
 @AStyle\bin\astyle.exe -v --options=astyle-options.ini --recursive "../neo/*.h" --recursive "../neo/*.cpp" --recursive "../neo/*.c"
+@rem Apply Styles to "Doom Classic"
+@AStyle\bin\astyle.exe -v --options=astyle-options.ini --recursive "../doomclassic/*.cpp" --recursive "../doomclassic/*.h"
 @echo "Uniform style successfully added to both Header files and source files."
 pause
